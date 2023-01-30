9edd77efd9e91e14af6d4cd56fc6235b10aed51d
Quoc PHAN <phquoc25@gmail.com>
2023-01-30 10:55:12 +0700

    add webpack dependencies

--- src/example.spec.js
+++ src/example.spec.js
@@ -1,6 +1,5 @@
 describe('my first test', () => {
     it('shoudl fail the test', () => {
-        main();
         fail('hehe');
     });
 });
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export function calculatePrice() {
+    console.log('Hello there!');
+}
